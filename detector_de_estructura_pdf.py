cfbb3f5a6311370d2e93ba23667d12c7d59adb6b
Ronald Garbanzo <ronald.gc3154@gmail.com>
2024-06-26 17:57:11 -0600

    Se_añadio_la_funcion_de_seleccionar_y_salir

--- detector_de_estructura_pdf.py
+++ detector_de_estructura_pdf.py
@@ -1,13 +1,64 @@
-import fitz  # PyMuPDF
+import fitz  
 import pdfplumber
+import sys
+import os
 
+
+#Carpeta donde estan los archivos 
 def pdfs_dir():
-    directorio = "C:/Users/Ronald/Downloads/Universidad_pdf/Ing de software M22T5.pdf"
+    directorio = "C:/Users/Ronald/Downloads/Pruebas"
     return directorio
 
+#Crea lista de los archivos existentes 
+def lista_pdfs(pdfs):
+    if not os.path.exists(pdfs):
+        print("El directorio no existe.")
+        return []
+    archivos_pdf = [f for f in os.listdir(pdfs) if f.endswith(".pdf")]
+    if not archivos_pdf:
+        print("No se encontraron PDFs en la carpeta.")
+    return archivos_pdf
 
+#Función para seleccionar los pdfs encontrados
+def seleccion_pdf(pdfs):
+    print("Estos fueron los PDFs encontrados, ingresa el numero del pfd seleccionado")
+    for i, pdf in enumerate (pdfs, start=1):
+        print(f"{i}. {pdf}")
+
+    pdf = True
+    errores = 0
+
+    while pdf:
+        try:
+            seleccionado = int(input("Elige el PDF que quieras convertir:"))- 1 
+            if 0 <= seleccionado < len(pdfs):
+                return pdfs[seleccionado]
+            else:
+                errores += 1
+                print("No se pudo encontrar el número de ese pdf.")
+        except (ValueError, IndexError):
+            errores += 1
+        print("No se pudo encontrar el número de ese pdf.")
+
+        if errores == 3: 
+            salir_del_programa() 
+
+#Funcion para salir del programa
+def salir_del_programa():
+    respuesta = input("Quieres salir del programa? (Si/No):").strip().lower()
+    if respuesta =="si":
+        print('El programa se detuvo')
+        sys.exit()
+    elif respuesta == "no":
+       errores = 0
+       seleccion_pdf(lista_pdfs(pdfs_dir())) 
+    else:
+        print("La respuesta que diste es invalida, responde solamente con un Si o  un No ")
+        return salir_del_programa()
+
+# Abre el documento PDF mitz
 def inspect_pdf_structure(pdf_path):
-    # Abre el documento PDF con PyMuPDF
+    
     document = fitz.open(pdf_path)
 
     # Itera a través de todas las páginas del documento
@@ -55,6 +106,15 @@ def inspect_pdf_structure(pdf_path):
             
             print("\n" + "="*80 + "\n")
 
-# Ruta al archivo PDF
-pdf_path = pdfs_dir()
-inspect_pdf_structure(pdf_path)
+
+directorio_pdfs = pdfs_dir()
+pdfs_encontrados = lista_pdfs(directorio_pdfs)
+
+if pdfs_encontrados:
+    pdf_seleccionado = seleccion_pdf(pdfs_encontrados)
+    pdf_path = os.path.join(directorio_pdfs, pdf_seleccionado)
+    inspect_pdf_structure(pdf_path)
+    salir_del_programa()
+else:
+    print("No se encontro ningun archivo")
+
